01b5c0e832056748f650925e5d028352da9ce8d0
Nick Warters <nick.warters@gmail.com>
2022-12-30 20:43:52 +0000

    Day 2 Part 1

--- src/02/part1.ts
+++ src/02/part1.ts
@@ -1,6 +1,9 @@
 import { readFileSync } from 'fs'
+import { splitLines } from '../utils/helpers.js'
 
-const tests: [string, any][] = [[``, 0]]
+const tests: [string, any][] = [[`1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc`, 2]]
 
 tests.forEach(([input, expected], i) => {
     const result = solve(input)
@@ -9,7 +12,7 @@ tests.forEach(([input, expected], i) => {
 Expected: ${expected}
 Got: ${result}
 ----
-Test ${i + 1} ${result === expected ? 'Pass': 'Fail'}`)
+Test ${i + 1} ${result === expected ? 'Pass': 'Fail'}\n----\n----`)
 })
 
 console.log(`Full Input Solution\n----\n${solve(readFileSync('./input.txt', { encoding: 'utf-8' }))}`)
@@ -17,6 +20,35 @@ console.log(`Full Input Solution\n----\n${solve(readFileSync('./input.txt', { en
 function solve(input: string): number {
 
     let retVal = 0
+
+    let minmax: string | number[] = ''
+    let min: number | string = ''
+    let max: number | string = ''
+    let c = ''
+    let pw = ''
+
+    lineLoop:
+    for (const line of splitLines(input)){
+        [minmax, c, pw] = line.split(' ')
+
+        c = c.replace(':', '')
+        minmax = minmax.split('-')!.map(n => parseInt(n))
+        min = minmax[0]
+        max = minmax[1]
+
+        let count = 0
+        for(let i = 0; i < pw.length; i++){
+            if(pw.charAt(i) === c){
+                count++
+            }
+            if(count > max){continue lineLoop}
+        
+        }
+        if(count >= min && count <= max){
+            retVal++
+        }
+
+    }
 
     return retVal
 }
