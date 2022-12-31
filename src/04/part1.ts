ced266fa95b41fcdc9ae38266ce3ffcf5b698438
Nick Warters <nick.warters@gmail.com>
2022-12-31 15:39:58 +0000

    Day 4 Part 1

--- src/04/part1.ts
+++ src/04/part1.ts
@@ -1,6 +1,18 @@
 import { readFileSync } from 'fs'
 
-const tests: [string, any][] = [[``, 0]]
+const tests: [string, any][] = [[`ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in`, 2]]
 
 tests.forEach(([input, expected], i) => {
     const result = solve(input)
@@ -9,14 +21,22 @@ tests.forEach(([input, expected], i) => {
 Expected: ${expected}
 Got: ${result}
 ----
-Test ${i + 1} ${result === expected ? 'Pass': 'Fail'}`)
+Test ${i + 1} ${result === expected ? 'Pass': 'Fail'}\n----`)
 })
 
-console.log(`Full Input Solution\n----\n${solve(readFileSync('./input.txt', { encoding: 'utf-8' }))}`)
+console.log(`----\nFull Input Solution\n----\n${solve(readFileSync('./input.txt', { encoding: 'utf-8' }))}`)
 
 function solve(input: string): number {
 
     let retVal = 0
+
+    const fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+    for(const line of input.split('\n\n')){
+        if(fields.every(f => line.includes(`${f}:`))){
+            retVal++
+        }
+    }
 
     return retVal
 }
