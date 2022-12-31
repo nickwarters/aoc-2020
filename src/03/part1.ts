056845e20a8dabc9a4716ef4b23a8cf1b8edf73c
Nick Warters <nick.warters@gmail.com>
2022-12-31 00:29:07 +0000

    getting started with day 3 part 1

--- src/03/part1.ts
+++ src/03/part1.ts
@@ -1,6 +1,16 @@
 import { readFileSync } from 'fs'
 
-const tests: [string, any][] = [[``, 0]]
+const tests: [string, any][] = [[`..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`, 7]]
 
 tests.forEach(([input, expected], i) => {
     const result = solve(input)
@@ -17,6 +27,38 @@ console.log(`Full Input Solution\n----\n${solve(readFileSync('./input.txt', { en
 function solve(input: string): number {
 
     let retVal = 0
+
+    const step = input.indexOf('\n')
+
+    console.log({step})
+    input = input.replace(/\n/g, '')
+
+    let path = input.split('') 
+
+    let i = 0
+    let n = 2
+    while(true){
+
+        if(i > input.length){
+            break 
+        }
+
+        i += step + 3
+        if(i > n * step){
+            i = i - step
+        }
+
+        if(input.charAt(i) === '#'){
+            retVal++
+            path[i] = 'X'
+        } else {
+            path[i] = 'O'
+        }
+
+        n++
+    }
+
+    console.log({input, path})
 
     return retVal
 }
