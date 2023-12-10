09980ad3dad94384f13fc9de7015ad5f3fbc74e4
Nick Warters <nick.warters@gmail.com>
2023-12-10 22:22:34 +0000

    day 8 part 1

--- src/08/part1.ts
+++ src/08/part1.ts
@@ -1,22 +1,64 @@
 import { readFileSync } from 'fs'
 
-const tests: [string, any][] = [[``, 0]]
+const tests: [string, any][] = [
+	[
+		`nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`,
+		5,
+	],
+]
 
 tests.forEach(([input, expected], i) => {
-    const result = solve(input)
-    console.log(`Example Input Solution
+	const result = solve(input)
+	console.log(`Example Input Solution
 ----
 Expected: ${expected}
 Got: ${result}
 ----
-Test ${i + 1} ${result === expected ? 'Pass': 'Fail'}`)
+Test ${i + 1} ${result === expected ? 'Pass' : 'Fail'}`)
 })
 
-console.log(`Full Input Solution\n----\n${solve(readFileSync('./input.txt', { encoding: 'utf-8' }))}`)
+console.log(
+	`Full Input Solution\n----\n${solve(
+		readFileSync(__dirname + '/' + 'input.txt', { encoding: 'utf-8' })
+	)}`
+)
 
 function solve(input: string): number {
+	const lines = input.split('\n')
 
-    let retVal = 0
+	let total = 0
+	let i = 0
+	const seen = new Set()
 
-    return retVal
+	while (true) {
+		if (seen.has(i)) {
+			break
+		}
+		seen.add(i)
+		const [inst, num] = lines[i].split(' ')
+		if (inst === 'nop') {
+			i++
+			continue
+		}
+		if (inst === 'jmp') {
+			console.log(`jumping ${num} to ${i + parseInt(num)}`)
+			i += parseInt(num)
+			continue
+		}
+
+		i++
+
+		total += parseInt(num)
+		console.log(`adding ${num}. new total=${total}`)
+	}
+
+	return total
 }
