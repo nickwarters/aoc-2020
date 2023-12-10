326fa9d5f391a5ed54dafdb0e436ed71e3c87f4d
Nick Warters <nick.warters@gmail.com>
2023-12-10 21:56:07 +0000

    day 7

--- src/07/part2.ts
+++ src/07/part2.ts
@@ -11,7 +11,17 @@ dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.`,
-		4,
+		32,
+	],
+	[
+		`shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`,
+		126,
 	],
 ]
 
@@ -31,20 +41,22 @@ console.log(
 	)}`
 )
 
+type Tree = Record<string, Record<string, number>>
+
 function solve(input: string): number {
 	let total = 0
 
-	const tree: Record<string, Record<string, number>> = {}
+	const tree: Tree = {}
 
 	input.split('\n').forEach((line) => {
 		const [bag, contains] = line.split(' bags contain ')
 		if (!Object.keys(tree).includes(bag)) {
 			tree[bag] = { 'shiny gold': 0 }
 		}
+		if (line.includes('no other bags')) {
+			return
+		}
 		contains.split(', ').forEach((item) => {
-			if (item.includes('no other bags')) {
-				return
-			}
 			const [count, bMod, bCol, _] = item.split(' ')
 			const b = [bMod, bCol].join(' ')
 			tree[bag][b] = tree[bag][b]
@@ -56,33 +68,33 @@ function solve(input: string): number {
 		})
 	})
 
-	const seenWithShinyGold = new Set<string>()
-	for (const bag in tree) {
-		let countOfShinyGold = tree[bag]['shiny gold']
-		if (countOfShinyGold) {
-			seenWithShinyGold.add(bag)
-			total += 1
+	console.log(tree)
+
+	let bag = 'shiny gold'
+
+	total = 0
+	const queue = Object.keys(tree[bag])
+	for (const b of queue) {
+		const m = tree[bag][b]
+
+		total += m * walkQueue(tree, b, m)
+	}
+
+	return total
+}
+
+function walkQueue(tree: Tree, bag: string, multiplier: number): number {
+	const queue = Object.entries(tree[bag]).filter(([_, v]) => v > 0)
+
+	let total = 1
+
+	for (const [b, _] of queue) {
+		const m = tree[bag][b]
+		if (m === 0) {
 			continue
 		}
 
-		let b: string | undefined
-		const queue: string[] = Object.keys(tree[bag])
-		while (countOfShinyGold || queue.length > 0) {
-			b = queue.pop()
-			if (b === undefined) {
-				break
-			}
-			countOfShinyGold += tree[b]['shiny gold']
-			if (countOfShinyGold) {
-				seenWithShinyGold.add(b)
-			}
-			queue.push(...Object.keys(tree[b]).filter((x) => x !== 'shiny gold'))
-		}
-
-		if (countOfShinyGold) {
-			seenWithShinyGold.add(bag)
-			total += 1
-		}
+		total += m * walkQueue(tree, b, m)
 	}
 
 	return total
